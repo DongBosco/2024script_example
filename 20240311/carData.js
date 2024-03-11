64f9aac45e1cc94712b5b941e7232186448f97dd
bosco <dk960227@hanmail.net>
2024-03-11 18:04:38 +0900

    20240311_1804_newVer

---
+++ 20240311/carData.js
@@ -0,0 +1,40 @@
+var carData=[
+    {
+        name : "avante",
+        speed : 10,
+        color : "white",
+        door : 4,
+        start : function(){
+            return this.speed+10;
+        },                
+    },
+    {
+        name : "sonata",
+        speed : 30,
+        color : "green",
+        door : 4,
+        start : function(){
+            return this.speed+10;
+        },                
+    },
+    {
+        name : "santafe",
+        speed : 50,
+        color : "white",
+        door : 4,
+        start : function(){
+            return this.speed+10;
+        },                
+    },
+    {
+        name : "morning",
+        speed : 10,
+        color : "white",
+        door : 4,
+        start : function(){
+            return this.speed+10;
+        },                
+    }
+]
+
+export default carData;
---
+++ 20240311/test.js
@@ -0,0 +1,11 @@
+var car = {
+    name : "avante",
+    speed : 10,
+    color : "white",
+    door : 4,
+    start : function(){
+        return this.speed;
+    }
+}
+
+console.log(car.name);

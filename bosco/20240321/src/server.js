3199a6429064acfb9971bd8c23296c6be2828a3c
bosco <dk960227@hanmail.net>
2024-03-21 14:45:13 +0900

    20249321_1445

---
+++ bosco/20240321/src/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const { default: mongoose } = require('mongoose');
+// const dotenv = require('dotenv');
+const dotenv = require('dotenv');
+const { userRouter } = require('./routes/userRouter');
+const { blogRouter } = require('./routes/blogRouter');
+
+const MONGO_URL =
+    'mongodb+srv://dk960227:qwer1234@bosco.izxkqmx.mongodb.net/?retryWrites=true&w=majority&appName=bosco';
+dotenv.config();
+
+const server = async function () {
+    try {
+        await mongoose.connect(MONGO_URL); //promise
+        console.log('db connected');
+        app.use(express.json());
+        app.get('/', async (req, res) => {
+            try {
+                res.send('helloWorld');
+            } catch (error) {
+                return res.status(500).send({ error: error.message });
+            }
+        });
+        app.use('/user', userRouter);
+        app.use('/blog', blogRouter);
+        app.listen(3000);
+    } catch (error) {
+        console.log('잘못연결');
+        console.log({ error: error.message });
+    }
+};
+server();
